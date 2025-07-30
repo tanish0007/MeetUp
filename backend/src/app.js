d92d64192ad30efd03a9ae91781877259c5ba74c
tanish0007 <jangratanish0007@gmail.com>
2025-07-30 14:14:49 +0530

    User routes and controllers are done

--- backend/src/app.js
+++ backend/src/app.js
@@ -3,12 +3,15 @@ const app = express();
 import dotenv from 'dotenv';
 dotenv.config();
 import cookieParser from 'cookie-parser';
-import authRoutes from './routes/auth.route.js'
+import authRoutes from './routes/auth.route.js';
+import userRoutes from './routes/user.route.js'
 import { connectDB } from './lib/db.js';
 
 app.use(express.json());
 app.use(cookieParser());
+
 app.use('/api/auth', authRoutes);
+app.use('/api/users', userRoutes);
 
 app.listen(process.env.PORT || 5001, () => {
     console.log(`\nServer is running on http://localhost:${process.env.PORT || 5001}`);
---
+++ backend/src/routes/user.route.js
@@ -0,0 +1,18 @@
+import express from 'express';
+import { protectRoute } from '../middleware/auth.middleware.js';
+import { getMyFriends, getRecommendedUsers, sendFriendRequest, acceptFriendRequest , getFriendRequests, getOutgoingFriendReqs} from '../controllers/user.controller.js';
+
+const router = express.Router();
+
+router.use(protectRoute);
+
+router.get('/' , getRecommendedUsers);
+router.get('/friends' , getMyFriends);
+
+router.post('/frined-request/:id', sendFriendRequest);
+router.put('/frined-request/:id/accept', acceptFriendRequest);
+
+router.get('/friend-requests', getFriendRequests);
+router.get("/outgoing-friend-requests", getOutgoingFriendReqs);
+
+export default router;

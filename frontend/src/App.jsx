942aa12499125112d35a5f49a0e88da1b4b95126
tanish0007 <jangratanish0007@gmail.com>
2025-07-30 17:24:39 +0530

    Creating the pages of frontend"

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,29 @@
 import React from 'react'
+import { Routes, Route } from 'react-router';
+
+import HomePage from './pages/HomePage.jsx';
+import LoginPage from './pages/LoginPage.jsx';
+import SignUpPage from './pages/SignUpPage.jsx';
+import ChatPage from './pages/ChatPage.jsx';
+import CallPage from './pages/CallPage.jsx';
+import OnboardingPage from './pages/OnboardingPage.jsx';
+import NotificationsPage from './pages/NotificationsPage.jsx';
+
+import toast, { Toaster } from 'react-hot-toast';
 
 const App = () => {
   return (
-    <div className="h-screen App">
-
+    <div className="h-screen" data-theme="night">
+      <Routes>
+        <Route path='/' element={<HomePage />} />
+        <Route path='/signup' element={<SignUpPage /> } />
+        <Route path='/login' element={<LoginPage />} />
+        <Route path='/notifications' element={<NotificationsPage />} />
+        <Route path='/call' element={<CallPage />} />
+        <Route path='/chat' element={<ChatPage />} />
+        <Route path='/onboarding' element={<OnboardingPage />} />
+      </Routes>
+      <Toaster />
     </div>
   )
 }
---
+++ frontend/src/pages/ChatPage.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const ChatPage = () => {
+  return (
+    <div>
+      Chatpage
+    </div>
+  )
+}
+
+export default ChatPage

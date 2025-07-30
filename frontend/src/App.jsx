e4f436aab7c405bea2745f7f49aa05e209fecbcd
tanish0007 <jangratanish0007@gmail.com>
2025-07-30 22:07:56 +0530

    Signup Page done

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,4 @@
-import React from 'react'
-import { Routes, Route } from 'react-router';
+import { Routes, Route, Navigate } from 'react-router';
 
 import HomePage from './pages/HomePage.jsx';
 import LoginPage from './pages/LoginPage.jsx';
@@ -9,19 +8,32 @@ import CallPage from './pages/CallPage.jsx';
 import OnboardingPage from './pages/OnboardingPage.jsx';
 import NotificationsPage from './pages/NotificationsPage.jsx';
 
-import toast, { Toaster } from 'react-hot-toast';
+import { Toaster } from 'react-hot-toast';
+import { useQuery } from '@tanstack/react-query';
+
+import { axiosInstance } from './lib/axios.js';
 
 const App = () => {
+  const { data: authData, isLoading, error } = useQuery({
+      queryKey: ['authUser'],
+      queryFn: async () => {
+        const response = await axiosInstance.get('/auth/me');
+        return response.data;
+      }
+  });
+
+  const authUser = authData?.user;
+  
   return (
     <div className="h-screen" data-theme="night">
       <Routes>
-        <Route path='/' element={<HomePage />} />
-        <Route path='/signup' element={<SignUpPage /> } />
-        <Route path='/login' element={<LoginPage />} />
-        <Route path='/notifications' element={<NotificationsPage />} />
-        <Route path='/call' element={<CallPage />} />
-        <Route path='/chat' element={<ChatPage />} />
-        <Route path='/onboarding' element={<OnboardingPage />} />
+        <Route path='/' element={ authUser ? <HomePage /> : <Navigate to='/login' /> } />
+        <Route path='/signup' element={ !authUser ? <SignUpPage /> : <Navigate to='/' /> } />
+        <Route path='/login' element={ !authUser ? <LoginPage /> : <Navigate to='/' />} />
+        <Route path='/notifications' element={ authUser ? <NotificationsPage /> : <Navigate to='/login' /> } />
+        <Route path='/call' element={ authUser ? <CallPage /> : <Navigate to='/login' /> } />
+        <Route path='/chat' element={ authUser ? <ChatPage /> : <Navigate to='/login' /> } />
+        <Route path='/onboarding' element={ authUser ? <OnboardingPage /> : <Navigate to='/login' /> } />
       </Routes>
       <Toaster />
     </div>
--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -4,10 +4,18 @@ import './index.css'
 import App from './App.jsx'
 import { BrowserRouter } from 'react-router';
 
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+
+const queryCLient = new QueryClient();
+
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <BrowserRouter>
-      <App />
+      <QueryClientProvider client={queryCLient}>
+
+        <App />
+
+      </QueryClientProvider>
     </BrowserRouter>
   </StrictMode>,
 )

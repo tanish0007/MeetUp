05bfc5875e7c9f332adb5b64530393b745a03cce
tanish0007 <jangratanish0007@gmail.com>
2025-07-31 20:56:30 +0530

    managing chats between user

---
+++ frontend/src/components/CallButton.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const CallButton = () => {
+  return (
+    <div>CallButton</div>
+  )
+}
+
+export default CallButton;
--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,5 +1,7 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+// might use v2
+import 'stream-chat-react/dist/css/index.css';
 import './index.css'
 import App from './App.jsx'
 import { BrowserRouter } from 'react-router';

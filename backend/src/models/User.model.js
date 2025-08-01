0c2c1254f941293f6641f4895db67ed67c35e5d1
tanish0007 <jangratanish0007@gmail.com>
2025-08-01 11:05:54 +0530

    change the styling of User.model.js

--- backend/src/models/User.model.js
+++ backend/src/models/User.model.js
@@ -2,15 +2,44 @@ import mongoose from "mongoose";
 import bcrypt from 'bcryptjs';
 
 const userSchema = new mongoose.Schema({
-    fullName: { type: String, required: true },
-    email: { type: String, required: true, unique: true },
-    password: { type: String, required: true, minlength: 8 },
-    bio: { type: String, default: "" },
-    profilePic: { type: String, default: "" },
-    nativeLanguage: { type: String, default: "" },
-    learningLanguage: { type: String, default: "" },
-    location: { type: String, default: "" },
-    isOnboarded: { type: Boolean, default: false },
+    fullName: {
+        type: String, 
+        required: true 
+    },
+    email: { 
+        type: String, 
+        required: true, 
+        unique: true 
+    },
+    password: { 
+        type: String, 
+        required: true, 
+        minlength: 8 
+    },
+    bio: { 
+        type: String, 
+        default: "" 
+    },
+    profilePic: { 
+        type: String, 
+        default: "" 
+    },
+    nativeLanguage: { 
+        type: String, 
+        default: "" 
+    },
+    learningLanguage: { 
+        type: String, 
+        default: "" 
+    },
+    location: { 
+        type: String, 
+        default: "" 
+    },
+    isOnboarded: { 
+        type: Boolean, 
+        default: false 
+    },
     friends: [{
         type: mongoose.Schema.Types.ObjectId,
         ref: "User"
@@ -23,8 +52,8 @@ userSchema.pre("save", async function (next) {
     try{
         const salt = await bcrypt.genSalt(10);
         this.password = await bcrypt.hash(this.password, salt);
-
         next();
+        
     } catch (err){
         next(err);
     }

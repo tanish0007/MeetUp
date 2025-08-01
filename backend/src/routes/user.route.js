ab0d939b1b1465d78c988e76d400a3f2f3f7d843
tanish0007 <jangratanish0007@gmail.com>
2025-08-01 15:36:39 +0530

    remove error from user routes

--- backend/src/routes/user.route.js
+++ backend/src/routes/user.route.js
@@ -9,8 +9,8 @@ router.use(protectRoute);
 router.get('/' , getRecommendedUsers);
 router.get('/friends' , getMyFriends);
 
-router.post('/frined-request/:id', sendFriendRequest);
-router.put('/frined-request/:id/accept', acceptFriendRequest);
+router.post('/friend-request/:id', sendFriendRequest);
+router.put('/friend-request/:id/accept', acceptFriendRequest);
 
 router.get('/friend-requests', getFriendRequests);
 router.get("/outgoing-friend-requests", getOutgoingFriendReqs);

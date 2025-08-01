09e785d1130ebc916e44dbc7eb87004bf1b24936
tanish0007 <jangratanish0007@gmail.com>
2025-08-01 11:31:28 +0530

    typofix in main.jsx

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,11 +1,11 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-// might use v2
+
 import 'stream-chat-react/dist/css/v2/index.css';
 import './index.css'
 import App from './App.jsx'
-import { BrowserRouter } from 'react-router';
 
+import { BrowserRouter } from 'react-router';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 
 const queryCLient = new QueryClient();
@@ -19,5 +19,5 @@ createRoot(document.getElementById('root')).render(
 
       </QueryClientProvider>
     </BrowserRouter>
-  </StrictMode>,
+  </StrictMode>
 )

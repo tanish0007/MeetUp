9907f2c7b7580d37969fedf541b84141310366bc
tanish0007 <jangratanish0007@gmail.com>
2025-08-02 12:43:45 +0530

    CallButton color

--- frontend/src/components/CallButton.jsx
+++ frontend/src/components/CallButton.jsx
@@ -3,7 +3,7 @@ import { VideoIcon } from "lucide-react";
 function CallButton({ handleVideoCall }) {
   return (
     <div className="p-3 border-b flex items-center justify-end max-w-7xl mx-auto w-full absolute top-0">
-      <button onClick={handleVideoCall} className="btn btn-success btn-sm text-white">
+      <button onClick={handleVideoCall} className="btn bg-black btn-success btn-sm text-white">
         <VideoIcon className="size-6" />
       </button>
     </div>
